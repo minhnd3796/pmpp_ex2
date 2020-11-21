18ccb21b4348ce18c8e390c40d02b706298127b7
Minh Nguyen <gordonnguyen3796@gmail.com>
2020-11-21 16:29:24 +0100

    first commit

---
+++ filtering.cc
@@ -0,0 +1,39 @@
+#include "filtering.h"
+#include "image.h"
+#include "common.h"
+#include "conv_cpu.h"
+#include "conv_gpu.h"
+
+void filtering(const char *imgfile, int ks)
+{
+	// === Task 1 ===
+	// TODO: Load image
+	// TODO: Generate gaussian filter kernel
+	// TODO: Blur image on CPU
+
+	// === Task 2 ===
+	// TODO: Blur image on GPU (Global memory)
+
+	// === Task 3 ===
+	// TODO: Blur image on GPU (Shared memory)
+
+	// === Task 4 ===
+	// TODO: Blur image on GPU (Constant memory)
+
+	// === Task 5 ===
+	// TODO: Blur image on GPU (L1/texture cache)
+
+	// === Task 6 ===
+	// TODO: Blur image on GPU (all memory types)
+}
+
+
+/************************************************************
+ * 
+ * TODO: Write your text answers here!
+ * 
+ * (Task 7) nvprof output
+ * 
+ * Answer: TODO
+ * 
+ ************************************************************/
